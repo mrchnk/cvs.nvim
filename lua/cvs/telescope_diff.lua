3a4f4a350d38c6bbc5ef2b7d55a3d08cb45c2dca
mrchnk <mrchnk@gmail.com>
2023-08-10 02:32:57 +0300

    search within diff's

--- lua/cvs/telescope_diff.lua
+++ lua/cvs/telescope_diff.lua
@@ -5,6 +5,16 @@ local sorters = require('telescope.sorters')
 local action_state = require('telescope.actions.state')
 local Previewer = require('telescope.previewers.previewer')
 local file_diff = require('cvs.file_diff')
+local fzy = require('telescope.algos.fzy')
+
+local ns = vim.api.nvim_create_namespace('cvs_telescope_diff')
+local higroup = 'Visual'
+--[[vim.api.nvim_set_hl(0, higroup, {
+  fg = '#dcd7ba',
+  bg = '#2d4f67',
+  bold = true,
+  default = false,
+})]]
 
 local function parse_diff(diff)
   local lines = vim.split(diff, "\n")
@@ -45,13 +55,21 @@ local function diff_dir()
   return result
 end
 
-local function entry_maker(entry)
+local function entry_maker(entry, matches)
   return {
     value = entry,
     filename = entry.file,
     ordinal = entry.file,
     display = entry.file,
+    matches = matches,
   }
+end
+
+local function highlight(buf, matches)
+  for _, m in ipairs(matches) do
+    local row, col, len = unpack(m)
+    vim.api.nvim_buf_add_highlight(buf, ns, higroup, row-1, col-1, col+len-1)
+  end
 end
 
 local function make_previewer()
@@ -63,12 +81,18 @@ local function make_previewer()
     end,
     teardown = function (self, status)
       local buf = self._buf
-      vim.api.nvim_buf_delete(buf, {force = true})
+      if buf then
+        vim.api.nvim_buf_delete(buf, {force = true})
+      end
     end,
     preview_fn = function (self, entry, status)
       local buf = self._buf
       vim.api.nvim_win_set_buf(status.preview_win, buf)
       vim.api.nvim_buf_set_lines(buf, 0, -1, true, entry.value.diff)
+      --vim.api.nvim_buf_clear_namespace(buf, ns, 0, -1)
+      if entry.matches then
+        highlight(buf, entry.matches)
+      end
     end,
   }
 end
@@ -82,16 +106,44 @@ local function on_select(bufnr)
   file_diff(file, {})
 end
 
+local function match(entry, prompt)
+  if not prompt or prompt == "" then
+    return true
+  end
+  local file_match = fzy.has_match(prompt, entry.file)
+  local matches = {}
+  for row, line in ipairs(entry.diff) do
+    local col = string.find(line, prompt, 2)
+    if col then
+      table.insert(matches, {row, col, string.len(prompt)})
+    end
+  end
+  return file_match or #matches > 0, matches
+end
+
+local function make_finder(results)
+  return setmetatable({
+    close = function () end,
+  }, {
+    __call = function (_, prompt, process_result, process_complete)
+      for _, entry in ipairs(results) do
+        local m, matches = match(entry, prompt)
+        if m then
+          process_result(entry_maker(entry, matches))
+        end
+      end
+      process_complete()
+    end,
+  })
+end
+
 return function (opts)
   local diff = diff_dir()
   local results = parse_diff(diff)
   pickers.new{
     prompt_title = "changed file",
-    finder = finders.new_table{
-      results = results,
-      entry_maker = entry_maker,
-    },
-    sorter = sorters.get_fzy_sorter{},
+    finder = make_finder(results),
+    sorter = sorters.highlighter_only{},
     preview_title = "diff",
     previewer = make_previewer(),
     attach_mappings = function(self, map)
