7285cd374e57652b17ef4e2d2849479e5576e0c0
mrchnk <mrchnk@gmail.com>
2023-08-14 02:54:07 +0300

    add highlights in diff previewer

--- lua/cvs/telescope/make_diff_previewer.lua
+++ lua/cvs/telescope/make_diff_previewer.lua
@@ -1,7 +1,33 @@
 local make_buf_previewer = require('cvs.telescope.make_buf_previewer')
 
+local function find_words(prompt, line)
+  local words = vim.gsplit(prompt, '%s+', {trimempty=true})
+  local word = words()
+  local pos = 0
+  return function ()
+    while word do
+      pos = string.find(line, word, pos+1)
+      if pos then
+        return pos, #word
+      end
+      word = words()
+      pos = 0
+    end
+  end
+end
+
 local function format_entry(entry, prompt)
-  return entry.value.body
+  local matches = {}
+  if #prompt > 0 then
+    for i, line in ipairs(entry.value.body) do
+      if i > 3 then
+        for pos, len in find_words(prompt, line) do
+          table.insert(matches, {i, pos, len})
+        end
+      end
+    end
+  end
+  return entry.value.body, matches
 end
 
 return function ()
