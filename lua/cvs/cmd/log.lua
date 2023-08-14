4a2bbbab5d56ea3efcd68654e1cf0c7da43839d7
mrchnk <mrchnk@gmail.com>
2023-08-14 09:32:15 +0300

    refactor cmd log

--- lua/cvs/cmd/log.lua
+++ lua/cvs/cmd/log.lua
@@ -1,8 +1,10 @@
-local cvs_log = require('cvs.log')
 local telescope_log = require('cvs.telescope.log')
 local parse_args = require('cvs.cmd.parse_args')
 
-return function (opts)
-  local files, opts = parse_args(opts.args)
-  telescope_log({files = files, opts = opts})
+return function (command_options)
+  local files, opts = parse_args(command_options.args)
+  telescope_log{
+    files = files,
+    opts = opts,
+  }
 end
