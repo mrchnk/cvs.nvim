1916d6c709cf7ad08d509f96568520e9ff9f9ef0
mrchnk <mrchnk@gmail.com>
2023-08-24 20:01:48 +0300

    extract cmd ids

--- lua/cvs/cmd/init.lua
+++ lua/cvs/cmd/init.lua
@@ -1,4 +1,12 @@
+local id = {
+  log = 'CVSLog',
+  diff = 'CVSDiff',
+  annotate = 'CVSAnnotate',
+  commit = 'CVSCommit',
+}
+
 return {
+  id = id,
   diff = require("cvs.cmd.diff"),
   log = require('cvs.cmd.log'),
   commit = require('cvs.cmd.commit'),
--- plugin/cvs.lua
+++ plugin/cvs.lua
@@ -3,25 +3,25 @@ local cvs_hl = require('cvs.ui.highlight')
 
 cvs_hl.setup()
 
-vim.api.nvim_create_user_command("CVSDiff", cmd.diff, {
+vim.api.nvim_create_user_command(cmd.id.diff, cmd.diff, {
   desc = 'Show diff for selected file(s) and revision(s) or cwd status',
   complete = 'file',
   nargs = '?',
 })
 
-vim.api.nvim_create_user_command("CVSLog", cmd.log, {
+vim.api.nvim_create_user_command(cmd.id.log, cmd.log, {
   desc = 'Show log for selected file(s) or cwd',
   complete = 'file',
   nargs = '?',
 })
 
-vim.api.nvim_create_user_command('CVSCommit', cmd.commit, {
+vim.api.nvim_create_user_command(cmd.id.commit, cmd.commit, {
   desc = 'Commit selected file(s) or staged changes',
   complete = 'file',
   nargs = '?',
 })
 
-vim.api.nvim_create_user_command('CVSAnnotate', cmd.annotate, {
+vim.api.nvim_create_user_command(cmd.id.annotate, cmd.annotate, {
   desc = 'Annotate current buffer',
   complete = 'file',
   nargs = '?'
