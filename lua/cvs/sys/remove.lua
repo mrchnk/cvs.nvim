e4e2d93cd5aafdfbfe10390662712bcd947ab895
mrchnk <mrchnk@gmail.com>
2024-03-22 09:15:32 +0300

    rename shell_error to code

--- lua/cvs/sys/remove.lua
+++ lua/cvs/sys/remove.lua
@@ -3,11 +3,11 @@ local run = require('cvs.sys.run')
 local function cvs_remove(file)
   local temp = vim.fn.tempname()
   vim.fn.rename(file, temp)
-  local _, shell_error = run{
+  local _, code = run{
     "remove", file
   }
   vim.fn.rename(temp, file)
-  if shell_error > 0 then
+  if code > 0 then
     error('CVS REMOVE: failed to remove ' .. file)
   end
 end
