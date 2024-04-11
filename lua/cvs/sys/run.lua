7c46cbd48faac41f1ccf046876c55663cf4c2977
mrchnk <mrchnk@gmail.com>
2024-04-11 09:18:11 +0300

    Use plenary to run sys

--- lua/cvs/sys/run.lua
+++ lua/cvs/sys/run.lua
@@ -1,3 +1,5 @@
+local Job = require('plenary.job')
+
 local function escape(arg)
   if string.find(arg, '[ <>]') then
     return '"' .. arg .. '"'
@@ -5,22 +7,50 @@ local function escape(arg)
   return arg
 end
 
-return function (args, opts)
+local function exec(args, opts)
   args = vim.tbl_map(escape, vim.tbl_flatten(args))
   opts = opts or {}
   local cmd = opts.cmd or 'cvs'
   cmd = cmd .. ' ' .. table.concat(args, ' ')
-  if opts.exec then
-    return vim.cmd('!' .. cmd)
-  end
-  local final_cmd = 'TZ=UTC ' .. cmd
-  if not opts.error_output then
-    final_cmd = final_cmd .. ' 2>/dev/null'
-  end
-  local lines = vim.fn.systemlist(final_cmd)
-  local code = vim.v.shell_error
-  if opts.expect_code and code ~= opts.expect_code then
-    error(string.format('CVS sys failed (%s) code=%d', cmd, code))
-  end
-  return lines, code
+  return vim.cmd('!' .. cmd)
 end
+
+local function find_command(name)
+  local command = vim.fn.systemlist('which ' .. name)
+  if vim.v.shell_error ~= 0 then
+    error(string.format('Command %s not found', name))
+  end
+  return command[1]
+end
+
+return function (args, opts)
+  args = args or {}
+  opts = opts or {}
+  if opts.exec then
+    return exec(args, opts)
+  end
+  local cmd = opts.cmd or 'cvs'
+  local stdout = {}
+  local stderr = {}
+  local job = Job:new{
+    command = find_command(cmd),
+    args = args,
+    env = { TZ = 'UTC' },
+    on_stdout = function (_, line)
+      table.insert(stdout, line)
+    end,
+    on_stderr = function (_, line)
+      if opts.error_output then
+        table.insert(stdout, line)
+      else
+        table.insert(stderr, line)
+      end
+    end,
+  }
+  local _, code = job:sync()
+  if opts.expect_code and code ~= opts.expect_code then
+    error(string.format('Command %s failed with code %d', cmd, code))
+  end
+  return stdout, code
+end
+
