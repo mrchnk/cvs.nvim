caa8563ff3de35911c89291c6dc1edf6459a291b
mrchnk <mrchnk@gmail.com>
2024-04-16 23:18:18 +0300

    fix annotate temp

--- lua/cvs/utils/build_temp.lua
+++ lua/cvs/utils/build_temp.lua
@@ -1,26 +1,24 @@
 return function (entries)
   local sorted = vim.tbl_extend('force', {}, entries)
   table.sort(sorted, function (a, b)
-    if not a.ts then return true end
-    if not b.ts then return false end
-    return a.ts < b.ts
+    return (a.ts or -1) < (b.ts or -1)
   end)
   local count = 0
   local last_rev
   for _, entry in ipairs(sorted) do
     if entry.rev then
-      entry.temp = count
       if entry.rev ~= last_rev then
-        last_rev = entry.rev
         count = count + 1
+        last_rev = entry.rev
       end
+      entry.temp = count
     end
   end
   vim.print({ c = count, l = last_rev })
   if count > 1 then
     for _, entry in ipairs(sorted) do
-      if entry.temp then
-        entry.temp = entry.temp / count
+      if entry.rev then
+        entry.temp = (entry.temp-1) / (count-1)
       end
     end
   end
